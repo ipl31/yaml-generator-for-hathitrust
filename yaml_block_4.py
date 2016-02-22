e54a1064c7ffde5745a64a5fd8f848f0a59ad9cb
Ruth Tillman <rtillman@lib-2085.library.nd.edu>
2016-02-22 09:35:29 -0500

    Added all inputs for page types and tests.

--- yaml_block_4.py
+++ yaml_block_4.py
@@ -1,6 +1,6 @@
-# Goal: Print list of images in proper YAML format.
-# Goal: Handle reading order.
+# Goal: To handle labels
 
+# Determines the length of the 000s to ensure that the filename is 8 characters.
 def determinePrefixLength(pageNum):
 	global prefixZeroes
 	if 0 < pageNum < 10:
@@ -13,33 +13,58 @@ def determinePrefixLength(pageNum):
 		prefixZeroes = '0000'
 	else:
 		prefixZeroes = 'error'
-		
+
+# Creates the file's name. Combines the leading 0s, integer as string, and filetype, and outputs global variable fileName
 def generateFileName(prefix, suffix, fileType):
 	global fileName
 	fileName = prefix + str(suffix) + '.' + fileType
 
+#  Uses the number of the reading start page to determine where the reading order starts/print.
 def generateOrderLabel(readingStartNum, pageNum, orderNum):
 	global orderLabel
 	orderLabel = ''
 	if pageNum >= readingStartNum:
 		orderLabel = 'orderlabel: "' + str(orderNum) + '"'
 
+# Handles the reading labels. Uses list function which then gets split apart, so that multiple labels can apply to same page if relevant.
 def generateLabel(pageNum):
 	global label
 	labelList = []
-	blankPagesList = map(int, blankPages.split(", "))
 # Multiple labels can be comma-separated...
 	if pageNum == frontCover:
 		labelList.append('"FRONT_COVER"')
 	if pageNum == backCover:
 		labelList.append('"BACK_COVER"')
-	if pageNum in blankPagesList:
+	if pageNum in blankPages:
 		labelList.append('"BLANK"')
+	if pageNum in chapterPages:
+		labelList.append('"CHAPTER_PAGE"')
+	if pageNum in chapterStart:
+		labelList.append('"CHAPTER_START"')
+	if pageNum in copyrightPages:
+		labelList.append('"COPYRIGHT"')
+	if pageNum in firstChapterStart:
+		labelList.append('"FIRST_CONTENT_CHAPTER_START"')
+	if pageNum in foldoutPages:
+		labelList.append('"FOLDOUT"')
+	if pageNum in imagePages:
+		labelList.append('"IMAGE_ON_PAGE"')
+	if pageNum in indexStart:
+		labelList.append('"INDEX"')
+	if pageNum in referenceStartPages:
+		labelList.append('"REFERENCES"')
+	if pageNum in tableOfContentsStarts:
+		labelList.append('"TABLE_OF_CONTENTS"')
+	if pageNum in titlePages:
+		labelList.append('"TITLE"')
+	if pageNum in halfTitlePages:
+		labelList.append('"TITLE_PARTS"')
 	if not labelList:
 		label = ''
 	else:
 		label = 'label: ' + ', '.join(labelList)
 
+# Combines all functions to write the file.
 def writeFile(finalNumber, readingStartNum, fileType, outputFile):
 	f = open(outputFile, 'w')
 	pageNum = 1
@@ -59,17 +84,30 @@ def writeFile(finalNumber, readingStartNum, fileType, outputFile):
 		pageNum += 1
 	f.close()
 
-
+# Putting input into a function vs. having a huge list of inputs at the end.
 def gatherInput():
-	global fileType, finalNumber, readingStartNum, frontCover, outputFile, backCover, blankPages
+	global fileType, finalNumber, readingStartNum, frontCover, outputFile, backCover, blankPages, chapterPages, chapterStart, copyrightPages, firstChapterStart, foldoutPages, imagePages, indexStart, multiworkBoundaries, prefacePages, referenceStartPages, tableOfContentsStarts, titlePages, halfTitlePages
 	outputFile = raw_input("What file to do you want to write this to? ")
 	fileType = raw_input("What is the (lowercase) filetype? ")
-	finalNumber = int(raw_input("What is the number of the final image? "))
+	finalNumber = input("What is the number of the final image? ")
 	print 'When listing multiple numbers, separate them with a ", ".\nSome entries, such as the first chapter, should only have multiple entries if multiple works are bound together and submitted together, generally volumes of the same title. Occasionally something like Index may be repeated within a single work, such as a hymnal.\nWhen a question has no answer, ENTER 0.'
-	readingStartNum = int(raw_input("What is the file number on which page 1 occurs? "))
-	frontCover = int(raw_input("What file number is the front cover? "))
-	backCover = int(raw_input("What is the file number of the back cover? "))
-	blankPages = raw_input("List the file numbers of any black pages: ")
+	readingStartNum = input("What is the file number on which page 1 occurs? ")
+	frontCover = input("What file number is the front cover? ")
+	backCover = input("What is the file number of the back cover? ")
+	blankPages = input("List the file numbers of any blank pages: ")
+	chapterPages = input("List the file numbers of pages containing only chapter names: ")
+	chapterStart = input("List file numbers of the start of each chapter **EXCEPT** the first, including appendices: ")
+	copyrightPages = input("List the file number of the title page verso (back of title page containing copyright info) for each work: ")
+	firstChapterStart = input("List the file number of the first chapter on a regularly-numbered page (may be Preface) for each work: ")
+	foldoutPages = input("List the file number of any page that is a scan of a foldout: ")
+	imagePages = input("List the file number of any page which is only an image: ")
+	indexStart = input("List the file number of any pages which are the FIRST page of an index: ")
+	multiworkBoundaries = input("List the file number of any multi-work boundaries: ")
+	prefacePages = input("List the file number of each Preface, defined as sections that appear between the title page verso/copyright and page 1. Do not list any Prefaces beginning on or after page 1: ")
+	referenceStartPages = input("List the file number of the first page of any set of references or bibliography: ")
+	tableOfContentsStarts = input("List file numbers of the first page of any Table of Contents: ")
+	titlePages = input("List file number of any title pages (one per work): ")
+	halfTitlePages = input("List the file numbers of any half title pages (preliminary title pages often before the first title page, little or no information on reverse): ")
 
 gatherInput()
 writeFile(finalNumber, readingStartNum, fileType, outputFile)
